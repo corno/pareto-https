72ac1b83d94a17cf1ee7577493fec4c7ac661983
corno <corno.astn.a@schraverus.com>
2022-06-30 21:58:41 +0200

    first commit

---
+++ lib/src/etc/https.ts
@@ -0,0 +1,40 @@
+import * as https from "https"
+import * as asyncAPI from "pareto-async-api"
+
+export function createLeafHTTPSCaller<T>(
+    hostname: string,
+    path: string,
+    onData: (data: string) => void,
+    onError: (e: Error) => void,
+    onEnd: () => T
+): asyncAPI.IAsync<T> {
+    return {
+        execute: (cb) => {
+            const options = {
+                hostname: hostname,
+                //port: 443,
+                path: path,
+                method: 'GET'
+            }
+
+            const req = https.request(options, res => {
+                //console.log(`statusCode: ${res.statusCode}`)
+
+
+                res.on('data', d => {
+                    onData(d)
+                })
+                res.on('end', () => {
+                    cb(onEnd())
+                })
+            })
+
+            req.on('error', error => {
+                onError(error)
+            })
+
+            req.end()
+        }
+
+    }
+}
