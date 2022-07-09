fbf397caf4713a108ec498cc0a1128ee004fa543
corno <corno.astn.a@schraverus.com>
2022-07-09 13:40:30 +0200

    .

--- api/src/interface/api.ts
+++ api/src/interface/api.ts
@@ -1,12 +1,12 @@
-import * as asyncAPI from "pareto-async-api"
+import * as pc from "pareto-api-core"
 
 export type Call = <T>(
     hostname: string,
     path: string,
     onData: (data: string) => void,
     onError: (e: null) => void,
-    onEnd: () => asyncAPI.IAsync<T>
-) => asyncAPI.IAsync<T>
+    onEnd: () => pc.IAsync<T>
+) => pc.IAsync<T>
 
 export type API = {
     call: Call
